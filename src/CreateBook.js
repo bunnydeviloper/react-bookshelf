5fccdf80c04c310ad9d99b1cdd1290257c5950c6
Sophia Mai <phuongmai001@gmail.com>
2018-10-08 15:03:15 -0700

    feat: createbook initialization ; ;

---
+++ src/CreateBook.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CreateBook extends React.Component {
+  render() {
+    return (
+      <div>Add more books here!</div>
+    )
+  }
+}
+
+export default CreateBook;
+
