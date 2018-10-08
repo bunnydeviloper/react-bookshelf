75c4ca894a4eedcb6ad4a9531843a906d67085e0
Sophia Mai <phuongmai001@gmail.com>
2018-10-08 16:20:56 -0700

    update display

--- src/DisplayAll.js
+++ src/DisplayAll.js
@@ -123,9 +123,6 @@ class DisplayAll extends React.Component {
             </div>
           </div>
         </div>
-        <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-        </div>
       </div>
     );
   }
